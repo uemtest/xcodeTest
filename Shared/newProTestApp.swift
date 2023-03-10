4fc5a6d84ab9749790fe022537ee5ecdcf816a51
“uemtest” <410657528@qq.com>
2023-03-10 16:02:50 +0800

    Initial Commit

---
+++ Shared/newProTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  newProTestApp.swift
+//  Shared
+//
+//  Created by sangfor on 2023/3/10.
+//
+
+import SwiftUI
+
+@main
+struct newProTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
